f3d30bb1293f0da81b0e1d7617d071a6025421d6
jisoleil <janggh1012@naver.com>
2023-06-03 13:40:57 +0900

    5.1

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,12 @@ const clock=document.querySelector("h2#clock");
 
 //clock.innerText="lolo";
 
-function sayHello()
+function getClock()
 {
-    console.log("hello");
+    const date= new Date();
+    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    
 
 }
-
-setInterval(sayHello,5000);
+getClock();
+setInterval(getClock,1000);
