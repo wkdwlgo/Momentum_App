d8c1354395830a233141ce07382431a9a76fbcd0
jisoleil <janggh1012@naver.com>
2023-06-03 13:52:54 +0900

    5.2

--- js/clock.js
+++ js/clock.js
@@ -5,9 +5,12 @@ const clock=document.querySelector("h2#clock");
 function getClock()
 {
     const date= new Date();
-    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours=String(date.getHours()).padStart(2,"0");//string로 형변환 pad 감싸기
+    const minutes=String(date.getMinutes()).padStart(2,"0");
+    const seconds=String(date.getSeconds()).padStart(2,"0");
+    clock.innerText=`${hours}:${minutes}:${seconds}`;
     
 
 }
-getClock();
+getClock();//1초 후에 보여주는게 아니라 바로 보여주기 위해 
 setInterval(getClock,1000);
