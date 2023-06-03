fd7bb95997bd0232a0b1f62f4d062c9e5ddf6f5d
jisoleil <janggh1012@naver.com>
2023-06-04 03:02:08 +0900

    7.1

--- js/todo.js
+++ js/todo.js
@@ -4,11 +4,27 @@ const toDoList=document.querySelector('#todo-list');
 
 const TODO='todo';
 
+function paintToDo(newTodo){
+    const li =document.createElement("li");
+    const span=document.createElement("span");
+    const button=document.createElement("button");
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText=newTodo;
+    button.innerText='Delete'; 
+    console.log(li);
+    toDoList.appendChild(li);
+
+}
+
+
+
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
-    console.log(toDoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
