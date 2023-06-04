9188a9abd1df5b64e69e6a7ea9bc1e206cfe3b2a
jisoleil <janggh1012@naver.com>
2023-06-05 02:25:13 +0900

    8.1

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+
+const api='e067c23109efc86fc64a24740ecc1c55';
+
 function onGeoOK(position){
     const lat=position.coords.latitude;
     const lng=position.coords.longitude;
     console.log("you live in",lat, lng);
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const nameContainer=document.querySelector("#weather span:nth-child(1)");
+        nameContainer.innerText=data.name;
+        const weatherContainer=document.querySelector("#weather span:nth-child(2)");
+        weatherContainer.innerText=data.weather[0].main;
+        const tempContainer=document.querySelector("#weather span:nth-child(3)");
+        tempContainer.innerText=` ${data.main.temp}°C`;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
